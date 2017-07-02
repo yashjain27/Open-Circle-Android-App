eea4a7e2b3475be590635b9f2792b5c7b5b983e6
Ariel Rodriguez <1998arielrodriguez@gmail.com>
2017-07-02 13:13:29 -0400

    initial facebook commit

--- app/src/main/java/tech/ceece/opencircle/Login.java
+++ app/src/main/java/tech/ceece/opencircle/Login.java
@@ -31,6 +31,9 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
 
 public class Login extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
     //Data fields
